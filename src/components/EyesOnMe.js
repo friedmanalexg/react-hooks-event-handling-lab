3806b4bc5bbf1833c442b6b92c9079407a4eaf35
Alex <friedmanalexg@gmail.com>
2022-07-22 10:43:46 -0400

    all tests spalshed, egress

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function handleBlur (){
+    console.log('Hey! Eyes on me!');
+    
+}  
+function handleFocus (){
+    console.log('Good!') 
+}   
+
+function renderEyesButton(){
+
+    return <button onFocus={handleFocus} onBlur={handleBlur} >Eyes on me</button>;
+} 
+
+export default renderEyesButton;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
+
+function handleKeypad () {
+    console.log("Entering password...")
+} 
 
 function Keypad (){
     return (
-        <div></div>
+        <div> <input onChange={handleKeypad}  type="password" />  </div>
     )
 }
 
